55177e00dcd49a82dde5313e84f0ad34fc920741
pikapikapow <vladimirhwang84@gmail.com>
2023-11-22 14:01:51 -0500

    Sliding and merging demo

--- app.js
+++ app.js
@@ -55,10 +55,41 @@ document.addEventListener("keyup", (e) => {
   }
 })
 
+function filterZero(row) {
+  return row.filter(num => num != 0)
+}
+
+function slide(row) {
+  row = filterZero(row);
+  for (let i = 0; i < row.length - 1; i++) {
+    if (row[i] == row[i + 1]) {
+      row[i] *= 2
+      row[i + 1] = 0
+      score += row[i]
+    }
+  }
+
+  row = filterZero(row)
+
+  while (row.length < columns) {
+    row.push(0)
+  }
+
+  return row;
+
+}
+
 function slideLeft() {
   for (let r = 0; r < rows; r++) {
     let row = board[r]
     row = slide(row)
     board[r] = row;
+
+    for (let c = 0; c < columns; c++) {
+      let tile = document.getElementById(r.toString() + "-" + c.toString())
+      let num = board[r][c]
+      updateTile(tile, num)
+    }
+
   }
 }
